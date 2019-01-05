fbf88efe13eb40607c16f08139027f4b0a9fe613
anuj8marc <46404917+anuj8marc@users.noreply.github.com>
2019-01-06 00:23:19 +0530

    Add files via upload

---
+++ IfElseExample.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.io.*;
+public class IfElseExample
+{
+public static void main (String args[])
+{
+int number=18;
+if (number%2==0)
+{
+System.out.println("even number");
+}
+else
+{
+System.out.println("odd number");
+}
+}
+}
